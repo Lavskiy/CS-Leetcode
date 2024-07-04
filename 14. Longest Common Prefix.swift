17b86c1577389c2c31fb8c79021b4cec42beb4a8
Peter Lavskiy <p.lavskiy.dev@gmail.com>
2024-07-03 21:07:32 -0300

    Solved LongestCommonPrefix

---
+++ 14. Longest Common Prefix.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+public enum LongestCommonPrefix {
+    
+    /*
+     Write a function to find the longest common prefix string amongst an array of strings.
+
+     If there is no common prefix, return an empty string "".
+
+      
+
+     Example 1:
+
+     Input: strs = ["flower","flow","flight"]
+     Output: "fl"
+     Example 2:
+
+     Input: strs = ["dog","racecar","car"]
+     Output: ""
+     Explanation: There is no common prefix among the input strings.
+     */
+    
+    public static func longestCommonPrefix(_ strs: [String]) -> String {
+        strs.reduce(into: strs.first ?? "") {
+            $0 = $1.commonPrefix(with: $0)
+        }
+    }
+}
