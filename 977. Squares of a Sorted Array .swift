e1223814b8749b1f254d8610c6ae07a5830b5f6b
Peter Lavskiy <p.lavskiy.dev@gmail.com>
2024-07-07 20:28:57 -0300

    Added two pointers solution for 997 problem

--- 977. Squares of a Sorted Array .swift
+++ 977. Squares of a Sorted Array .swift
@@ -16,4 +16,35 @@ public enum SquaresOfSortedArray  {
         
         return Sort.insertionSort(squares)
     }
+    
+    //let input = [-4,-1,0,3,10]
+    //let Output = [0,1,9,16,100]
+
+    //let input = [-5,-3,-2,-1]
+    //let Output = [1,4,9,25]
+    
+    public static func twoPointers(_ nums: [Int]) -> [Int] {
+        var i = nums.count - 1
+        var left = 0
+        var right = nums.count - 1
+        var result = nums
+
+        while i >= 0 {
+            var sqr: Int = 0
+            
+            if abs(nums[left]) > abs(nums[right]) {
+                sqr = nums[left]
+                left += 1
+            } else {
+                sqr = nums[right]
+                right -= 1
+            }
+            
+            result[i] = sqr * sqr
+            
+            i -= 1
+        }
+        
+        return result
+    }
 }
