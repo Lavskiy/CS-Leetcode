02c0be815c5a013857b0d020b3ab03b061e58727
Peter Lavskiy <p.lavskiy.dev@gmail.com>
2024-07-24 19:19:58 -0300

    Solved 209. Minimum Size Subarray Sum

---
+++ 209. Minimum Size Subarray Sum.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+class Solution {
+    /*
+     Given an array of positive integers nums and a positive integer target, return the minimal length of a
+     subarray
+      whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+      
+
+     Example 1:
+
+     Input: target = 7, nums = [2,3,1,2,4,3]
+     Output: 2
+     Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+     Example 2:
+
+     Input: target = 4, nums = [1,4,4]
+     Output: 1
+     Example 3:
+
+     Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+     Output: 0
+     */
+    func minSubArrayLen(_ target: Int, _ nums: [Int]) -> Int {
+        var left = 0
+        var sum = 0
+        var minLength = Int.max
+        
+        for right in 0..<nums.count {
+            sum += nums[right]
+            
+            while sum >= target {
+                minLength = min(minLength, right - left + 1)
+                sum -= nums[left]
+                left += 1
+            }
+        }
+        
+        return minLength == Int.max ? 0 : minLength
+    }
+}
